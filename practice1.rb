b200ceee68a49525768fb5f1390535b909bd62d5
Ivan <ivan@ivans-mbp.ait-sydney.net.au>
2016-08-09 14:42:33 +1000

    first commit

---
+++ practice1.rb
@@ -0,0 +1,23 @@
+class Roman
+  def initialize (country, emperors, time, people)
+    @country = country
+    @emperors = emperors
+    @time = time
+    @people = people
+  end
+
+    def country
+      @country = Italy
+    end
+
+    def emperors
+      @emperors = Nero
+    end
+
+    def time
+      @time = 2000
+    end
+
+    def people
+      @people = Romans
+    
